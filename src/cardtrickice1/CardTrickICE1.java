d95a64f44f6f3358d952caabb2e82c1ec53979ac
kau11901 <kau11901@sheridancollege.ca>
2022-06-02 18:28:43 -0400

    Searching luckyCard in magicHand and displaying a magicHand.

--- src/cardtrickice1/CardTrickICE1.java
+++ src/cardtrickice1/CardTrickICE1.java
@@ -5,11 +5,13 @@ Student id: 991662988
  * and open the template in the editor.
  */
 package cardtrickice1;
+
 import java.util.Scanner;
 
-/** step1 : generate 7 random cards and store in array - how
- * step 2: take any card input from user suit,number
- * step 3: user card is in  the array 'card is found'
+/**
+ * step1 : generate 7 random cards and store in array - how step 2: take any
+ * card input from user suit,number step 3: user card is in the array 'card is
+ * found'
  *
  * @author sivagamasrinivasan,May 23rd
  * @modifier Mehakpreet Kaur, June1
@@ -19,8 +21,7 @@ public class CardTrickICE1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7]; //Array of object
         Card luckyCard = new Card(); // Creating a luckyCard
         luckyCard.setValue(5);
@@ -33,12 +34,12 @@ public class CardTrickICE1 {
         }
         //step 2:take input 
         System.out.print("Enter a the card suit: {\"diamonds\",\"clubs\",\"spades\",\"hearts\"}");
-        String suit=in.next();    
-        System.out.print("Enter a the card number( between 1 and 13): "); 
-        int value=in.nextInt();
+        String suit = in.next();
+        System.out.print("Enter a the card number( between 1 and 13): ");
+        int value = in.nextInt();
         //step 3: match with array 
         boolean pal = false;
-        for(int i = 0; i< magicHand.length; i++){
+        for (int i = 0; i < magicHand.length; i++) {
             if (value == magicHand[i].getValue() && suit.equalsIgnoreCase(magicHand[i].getSuits())) {
                 pal = true;
                 break;
@@ -46,12 +47,31 @@ public class CardTrickICE1 {
                 pal = false;
             }
         }
-            if (pal) {
+        if (pal) {
             System.out.println("The card is In the Magic Hand");
-        } 
-            else {
+        } else {
             System.out.println("Sorry The card is not in the magic hand");
         }
+        
+
+        pal = false;//boolean for card
+        for (int i = 0; i < magicHand.length; i++) {
+            if (luckyCard.getValue() == magicHand[i].getValue() && luckyCard.getSuits().equalsIgnoreCase(magicHand[i].getSuits())) {
+                pal = true;
+                break;
+            } else {
+                pal = false;
+            }
+
+        }
+        if (pal) {
+            System.out.println("you won");
+        } else {
+            System.out.println("Sorry! u lost");
+        }
+        System.out.println("The magicHand : \n");
+        for(int i = 0; i<magicHand.length; i++){
+            System.out.println("Value:" + magicHand[i].getValue()+" Suits :" + magicHand[i].getSuits() ); // printing magicHands
         }
     }
-    
+}
